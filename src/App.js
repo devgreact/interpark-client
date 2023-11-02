24bdff7c6cc9066fa64c8dbd2803fc487c599208
devgreact <dev.greact@gmail.com>
2023-11-02 16:27:54 +0900

    feat : Swiper 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Header from "./components/Header";
+import Recommend from "./components/Recommend";
+import Visual from "./components/Visual";
 
 function App() {
   // 안녕하세요. js 자리입니다.
   /* 여러 줄 설명이에요 */
   return (
     <div className="wrap">
-
-
       {/* <!-- 상단 영역 --> */}
       <Header />
       {/* <!-- 메인 영역 --> */}
       <div className="main">
+        <Visual />
+        <Recommend />
       </div>
       {/* <!-- 하단 영역 --> */}
       <footer className="footer"></footer>
