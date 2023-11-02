ca6258c46af040c0a16173e32e71fe6207f17d3f
devgreact <dev.greact@gmail.com>
2023-11-02 16:50:33 +0900

    feat : cors 진행중

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/users", {
+    createProxyMiddleware("/", {
       target: "https://port-0-interpark-cloud-server-12fhqa2blnp9i5uw.sel5.cloudtype.app",
       changeOrigin: true,
     })
